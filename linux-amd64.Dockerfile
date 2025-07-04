c22f826db29e46ec4744c5e8aedc702e9477452a
engels74 <141435164+engels74@users.noreply.github.com>
2025-07-04 22:46:30 +0200

    feat(libtorrent): Add multi-libtorrent support
    - Introduce build arguments for multiple libtorrent versions
    - Download and stage qbittorrent-nox binaries for libtorrent v1 and v2
    - Update VERSION.json to store build revisions and full versions for both libraries
    - Add `LIBTORRENT` environment variable to select which libtorrent version to use
    - Modify `forwarded_port_update_app` to use selected qbittorrent-nox binary
    - Update `update-versions.sh` to fetch details for both libtorrent versions

--- linux-amd64.Dockerfile
+++ linux-amd64.Dockerfile
@@ -4,13 +4,18 @@ ARG UPSTREAM_DIGEST_AMD64
 FROM ${UPSTREAM_IMAGE}@${UPSTREAM_DIGEST_AMD64}
 EXPOSE 8080
 ARG IMAGE_STATS
-ENV IMAGE_STATS=${IMAGE_STATS} WEBUI_PORTS="8080/tcp,8080/udp"
+ENV IMAGE_STATS=${IMAGE_STATS} WEBUI_PORTS="8080/tcp,8080/udp" LIBTORRENT="v2"
 
 RUN ln -s "${CONFIG_DIR}" "${APP_DIR}/qBittorrent"
 
-ARG FULL_VERSION
-RUN curl -fsSL "https://github.com/userdocs/qbittorrent-nox-static/releases/download/${FULL_VERSION}/x86_64-qbittorrent-nox" > "${APP_DIR}/qbittorrent-nox" && \
-    chmod 755 "${APP_DIR}/qbittorrent-nox"
+ARG FULL_VERSION_LIB1
+ARG BUILD_REVISION_LIB1
+ARG FULL_VERSION_LIB2
+ARG BUILD_REVISION_LIB2
+RUN curl -fsSL "https://github.com/userdocs/qbittorrent-nox-static/releases/download/${FULL_VERSION_LIB1}/x86_64-qbittorrent-nox" > "${APP_DIR}/qbittorrent-nox-lib1" && \
+    chmod 755 "${APP_DIR}/qbittorrent-nox-lib1" && \
+    curl -fsSL "https://github.com/userdocs/qbittorrent-nox-static/releases/download/${FULL_VERSION_LIB2}/x86_64-qbittorrent-nox" > "${APP_DIR}/qbittorrent-nox-lib2" && \
+    chmod 755 "${APP_DIR}/qbittorrent-nox-lib2"
 
 ARG VUETORRENT_VERSION
 RUN curl -fsSL "https://github.com/vuetorrent/vuetorrent/releases/download/v${VUETORRENT_VERSION}/vuetorrent.zip" > "/tmp/vuetorrent.zip" && \
--- update-versions.sh
+++ update-versions.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 vuetorrent_version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/vuetorrent/vuetorrent/releases/latest" | jq -re .tag_name) || exit 1
-full_version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://github.com/userdocs/qbittorrent-nox-static/releases/latest/download/dependency-version.json" | jq -re '. | "release-\(.qbittorrent)_v\(.libtorrent_2_0)"') || exit 1
-version=$(sed -e "s/release-//g" -e "s/_.*//g" <<< "${full_version}")
+full_version_lib1=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://github.com/userdocs/qbittorrent-nox-static/releases/latest/download/dependency-version.json" | jq -re '. | "release-\(.qbittorrent)_v\(.libtorrent_1_2)"') || exit 1
+build_revision_lib1=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://github.com/userdocs/qbittorrent-nox-static/releases/download/${full_version_lib1}/dependency-version.json" | jq -re '.revision') || exit 1
+full_version_lib2=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://github.com/userdocs/qbittorrent-nox-static/releases/latest/download/dependency-version.json" | jq -re '. | "release-\(.qbittorrent)_v\(.libtorrent_2_0)"') || exit 1
+build_revision_lib2=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://github.com/userdocs/qbittorrent-nox-static/releases/download/${full_version_lib2}/dependency-version.json" | jq -re '.revision') || exit 1
+version=$(sed -e "s/release-//g" -e "s/_.*//g" <<< "${full_version_lib2}")
 [[ -z ${version} ]] && exit 0
 [[ ${version} == null ]] && exit 0
 json=$(cat VERSION.json)
 jq --sort-keys \
     --arg version "${version//v/}" \
-    --arg full_version "${full_version}" \
+    --arg full_version_lib1 "${full_version_lib1}" \
+    --arg build_revision_lib1 "${build_revision_lib1}" \
+    --arg full_version_lib2 "${full_version_lib2}" \
+    --arg build_revision_lib2 "${build_revision_lib2}" \
     --arg vuetorrent_version "${vuetorrent_version//v/}" \
-    '.version = $version | .full_version = $full_version | .vuetorrent_version = $vuetorrent_version' <<< "${json}" | tee VERSION.json
+    '.version = $version | .full_version_lib1 = $full_version_lib1 | .build_revision_lib1 = $build_revision_lib1 | .full_version_lib2 = $full_version_lib2 | .build_revision_lib2 = $build_revision_lib2 | .vuetorrent_version = $vuetorrent_version' <<< "${json}" | tee VERSION.json
